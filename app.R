5363fec37f056ef90b59e7e14afb9071a3d2f8de
Jennifer Slater <jslater5@binghamton.edu>
2026-07-28 19:21:40 -0400

    Update app.R

--- app.R
+++ app.R
@@ -10,57 +10,57 @@ ui<- page_fluid(
         background-color: #e7f1dc;}
                     "))),
   
-    navbarPage(title="Moth Morphometrics!", #ok this is cute :D
-               tabPanel("Home",
-                        sidebarLayout(
-                          sidebarPanel(
-                            #Sliders 
-                            fileInput("csv_please",label="Upload your CSV file"),
-                            "Box your measurment tool in!",
-                            sliderInput("x_position", "Change the X position:", min = 0, max = 500, value = 50),
-                            sliderInput("y_position", "Change the Y position:", min = 0, max = 500, value = 50),
-                            sliderInput("box_width", "Box width:", min = 0, max = 500, value = 50),
-                            sliderInput("box_height", "Box height:", min = 0, max = 500, value = 50),
-                            "",
-                            numericInput("true_width","What the width of your measurment tool?", value=NA),
-                            numericInput("true_height","What the heigth of your measurment tool?",value =NA),
-                            actionButton("submit","Submit the measurments")),
-                          
-                          mainPanel(
-                            tableOutput("results"),
-                            card(
-                              card_header("WOAH"),
-                              "hello Hello Hello!",
-                              
-                              div(
-                                style = "width: 500px; height: 500px;",
-                                uiOutput("moth_image"),
-                                uiOutput("dynamic_floating_box")),
-                              card_footer("more stuff"))
-                            
-                            ))),
-               tabPanel("Guide",
-                        "There is stuff in here.\n",
-                        "I just want to have lotsss of info on how to create the csv.",
-                        "Have picture files, under a column names Moth_URL.",
-                        "The first image will pop up on home screen, it should be a full picture of the setup.",
-                        "box the measuring tool to the best of your abilities.",
-                        "write the true measurments in whatever unit you want cm, in...anything.",
-                        "Maby have extra info about possible moth species based on location"
-                        ),
-               tabPanel("K-means clustering",
-                        "Submit the CSV you got from the HOME tab",
-                        fileInput("Home_csv",label="Upload your CSV file"),
-                        actionButton("color","Color"),
+  navbarPage(title="Moth Morphometrics!", #ok this is cute :D
+             tabPanel("Home",
+                      sidebarLayout(
+                        sidebarPanel(
+                          #Sliders 
+                          fileInput("csv_please",label="Upload your CSV file"),
+                          "Box your measurment tool in!",
+                          sliderInput("x_position", "Change the X position:", min = 0, max = 500, value = 50),
+                          sliderInput("y_position", "Change the Y position:", min = 0, max = 500, value = 50),
+                          sliderInput("box_width", "Box width:", min = 0, max = 500, value = 50),
+                          sliderInput("box_height", "Box height:", min = 0, max = 500, value = 50),
+                          "",
+                          numericInput("true_width","What the width of your measurment tool?", value=NA),
+                          numericInput("true_height","What the heigth of your measurment tool?",value =NA),
+                          actionButton("submit","Submit the measurments")),
                         
-                        ),
-               tabPanel("Timeline",
-                        "Submit the CSV you got from the HOME tab",
-                        fileInput("Home_csv",label="Upload your CSV file"),
-                        ),
-               
-               ))
-    
+                        mainPanel(
+                          tableOutput("results"),
+                          card(
+                            card_header("WOAH"),
+                            "hello Hello Hello!",
+                            
+                            div(
+                              style = "width: 500px; height: 500px;",
+                              uiOutput("moth_image"),
+                              uiOutput("dynamic_floating_box")),
+                            card_footer("more stuff"))
+                          
+                        ))),
+             tabPanel("Guide",
+                      "There is stuff in here.\n",
+                      "I just want to have lotsss of info on how to create the csv.",
+                      "Have picture files, under a column names Moth_URL.",
+                      "The first image will pop up on home screen, it should be a full picture of the setup.",
+                      "box the measuring tool to the best of your abilities.",
+                      "write the true measurments in whatever unit you want cm, in...anything.",
+                      "Maby have extra info about possible moth species based on location"
+             ),
+             tabPanel("K-means clustering",
+                      "Submit the CSV you got from the HOME tab",
+                      fileInput("Home_csv",label="Upload your CSV file"),
+                      actionButton("color","Color"),
+                      
+             ),
+             tabPanel("Timeline",
+                      "Submit the CSV you got from the HOME tab",
+                      fileInput("Home_csv",label="Upload your CSV file"),
+             ),
+             
+  ))
+
 
 # Server Function
 
@@ -97,7 +97,7 @@ server <- function(input, output) {
       src = firstimage()$Moth_URL[1],
       height = 500,
       width = 500,
-      style = "position: absolute;top: 0px;left: 0px;")
+      style = "position: absolute;top: 103px;left: 16px;")
     
   })
   
